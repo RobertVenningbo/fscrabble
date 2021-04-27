3b4d63ab7c5b298e6414a82dec82e1ec08709c90
RobertVenningbo <rove@itu.dk>
2021-04-27 14:34:06 +0200

    update

--- ScrabbleBot/StateMonad.fs
+++ ScrabbleBot/StateMonad.fs
@@ -6,27 +6,27 @@
         | IndexOutOfBounds of int
         | DivisionByZero 
         | ReservedName of string           
-
+    
     type Result<'a, 'b>  =
         | Success of 'a
         | Failure of 'b
-
+    
     type State = { vars     : Map<string, int> list
                    word     : (char * int) list 
                    reserved : Set<string> }
-
+    
     type SM<'a> = S of (State -> Result<'a * State, Error>)
-
+    
     let mkState lst word reserved = 
            { vars = [Map.ofList lst];
              word = word;
              reserved = Set.ofList reserved }
-
+    
     let evalSM (s : State) (S a : SM<'a>) : Result<'a, Error> =
         match a s with
         | Success (result, _) -> Success result
         | Failure error -> Failure error
-
+    
     let bind (f : 'a -> SM<'b>) (S a : SM<'a>) : SM<'b> =
         S (fun s ->
               match a s with
@@ -34,25 +34,29 @@
                 match f b with 
                 | S g -> g s'
               | Failure err     -> Failure err)
-
-
+    
+    
     let ret (v : 'a) : SM<'a> = S (fun s -> Success (v, s))
     let fail err     : SM<'a> = S (fun s -> Failure err)
-
+    
     let (>>=)  x f = bind f x
     let (>>>=) x f = x >>= (fun () -> f)
-
+    
     let push : SM<unit> = 
         S (fun s -> Success ((), {s with vars = Map.empty :: s.vars}))
-
-    let pop : SM<unit> = failwith "Not implemented"      
-
-    let wordLength : SM<int> = failwith "Not implemented"      
-
-    let characterValue (pos : int) : SM<char> = failwith "Not implemented"      
-
-    let pointValue (pos : int) : SM<int> = failwith "Not implemented"      
-
+    
+    let pop : SM<unit> = 
+        S (fun s -> Success ((), {s with vars = s.vars |> List.tail}))
+    
+    let wordLength : SM<int> = 
+        S (fun s -> Success (s.word.Length, s))
+    
+    let characterValue (pos : int) : SM<char> = 
+        S (fun s -> if s.word.Length <= pos || pos < 0 then Failure (IndexOutOfBounds pos) else Success(fst s.word.[pos],s))
+    
+    let pointValue (pos : int) : SM<int> = 
+        S (fun s -> if s.word.Length <= pos || pos < 0 then Failure (IndexOutOfBounds pos) else Success(snd s.word.[pos], s))
+    
     let lookup (x : string) : SM<int> = 
         let rec aux =
             function
@@ -61,11 +65,32 @@
                 match Map.tryFind x m with
                 | Some v -> Some v
                 | None   -> aux ms
-
+    
         S (fun s -> 
               match aux (s.vars) with
               | Some v -> Success (v, s)
               | None   -> Failure (VarNotFound x))
-
-    let declare (var : string) : SM<unit> = failwith "Not implemented"   
-    let update (var : string) (value : int) : SM<unit> = failwith "Not implemented"      
+    
+    
+    let declare (var : string) : SM<unit> = 
+        S (fun s -> 
+              match s.vars with
+              | [] -> Success ((), s)
+              | m::ms -> if Map.containsKey var m then Failure (VarExists var) 
+                         elif Set.contains var s.reserved then Failure (ReservedName var) 
+                         else Success ((), {s with vars = Map.add var 0 m::ms}))
+              
+    
+    let update (var : string) (value : int) : SM<unit> = 
+        let rec aux acc =
+            function
+            | [] -> None
+            | m :: ms ->
+                match Map.tryFind var m with 
+                | Some v -> Some (acc@([Map.add var value m])@ms) 
+                | None -> aux (acc@[m]) ms
+    
+        S (fun s -> 
+              match aux [] (s.vars) with
+              | Some v -> Success (() ,{s with vars = v})
+              | None   -> Failure (VarNotFound var))
